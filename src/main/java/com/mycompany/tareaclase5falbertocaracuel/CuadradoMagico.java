b01af4e7a42170143c3007ddf28491d65ad5d158
alberto <albertocaracuel16@gmail.com>
2021-03-03 13:32:00 +0100

    suma de diagonales

--- src/main/java/com/mycompany/tareaclase5falbertocaracuel/CuadradoMagico.java
+++ src/main/java/com/mycompany/tareaclase5falbertocaracuel/CuadradoMagico.java
@@ -21,7 +21,7 @@ public class CuadradoMagico {
                 matriz[x][y] = (int) (Math.random() * 8 + 1);
             }
         }
-        
+
     }
 
     public int[][] getMatriz() {
@@ -31,26 +31,70 @@ public class CuadradoMagico {
     public void setMatriz(int[][] matriz) {
         this.matriz = matriz;
     }
-    
-    public void sumaFila(){
-        int suma=0;
+
+    public void sumaFila() {
+        int suma = 0;
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
-                  suma += matriz[i][j];              
+                suma += matriz[i][j];
             }
             System.out.println("Fila " + i + " =" + suma);
-            suma=0;
+            suma = 0;
         }
     }
-    
-    public void sumaColumna(){
-        int suma=0;
+
+    public void sumaColumna() {
+        int suma = 0;
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
-                  suma += matriz[j][i];              
+                suma += matriz[j][i];
             }
             System.out.println("Columna " + i + " =" + suma);
-            suma=0;
+            suma = 0;
         }
+    }
+
+    private void visualizarArray(int[] pArray) {
+        for (int i = 0; i < pArray.length; i++) {
+            System.out.print("\t" + pArray[i]);
+        }
+    }
+
+    public void sumadiagonales() {
+        int[] diagoPrincipal = new int[matriz.length];
+        int[] diagoSecundaria = new int[matriz.length];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (i == j) {
+                    diagoPrincipal[i] = matriz[i][j];
+                }
+
+                if (i + j == matriz.length - 1) {
+                    diagoSecundaria[i] = matriz[i][j];
+                }
+            }
+        }
+        System.out.println("\nDiagonal Principal");
+        visualizarArray(diagoPrincipal);
+            int suma=0;
+
+        for (int i = 0; i < diagoPrincipal.length; i++) {
+          
+            suma += diagoPrincipal[i];
+
+        }
+        System.out.println("REsultado principal" + suma);
+
+        System.out.println("\n\nDiagonal Secundaria");
+        visualizarArray(diagoSecundaria);
+          int suma1=0;
+         for (int i = 0; i < diagoSecundaria.length; i++) {
+            
+            suma1 += diagoSecundaria[i];
+
+        }
+         
+         System.out.println("Resultado secundario" + suma1);
     }
 }
